4f2482b0283a94c75d5cb77f9e630f5d50798410
exochron <exochron@gamerstate.de>
2020-10-11 20:49:05 +0200

    1.1
    - toc update for 9.0
    - hide also shown slots in 9.0
    - use deployment on github

--- hide.lua
+++ hide.lua
@@ -1,4 +1,15 @@
-local frame = ContainerFrame1AddSlotsButton
-frame:Hide()
-frame.Show = frame.Hide
-frame.SetShown = frame.Hide
+local frames = {
+    ContainerFrame1AddSlotsButton,
+    ContainerFrame1ItemButton1,
+    ContainerFrame1ItemButton2,
+    ContainerFrame1ItemButton3,
+    ContainerFrame1ItemButton4,
+}
+
+for _, frame in ipairs(frames) do
+    if frame then
+        frame:Hide()
+        frame.Show = frame.Hide
+        frame.SetShown = frame.Hide
+    end
+end
