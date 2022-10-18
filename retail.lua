72fbdf971c2ae3295efc32b6a373a39555d184f2
exochron <exochron@freenet.de>
2022-10-18 14:46:40 +0200

    1.6.0 - updated retail logic for new bag system

--- retail.lua
+++ retail.lua
@@ -1,34 +1,53 @@
-
 local f = CreateFrame("Frame")
 f:RegisterEvent("PLAYER_ENTERING_WORLD")
 f:SetScript("OnEvent", function()
     C_CVar.SetCVarBitfield("closedInfoFrames", LE_FRAME_TUTORIAL_BAG_SLOTS_AUTHENTICATOR, true)
 end)
 
+ContainerFrame1.GetRows = function()
+    return IsAccountSecured() and 5 or 4
+end
+
+local function SortItemsByExtendedState(item1, item2)
+    local extended1, extended2 = item1:IsExtended(), item2:IsExtended();
+    if extended1 ~= extended2 then
+        return not extended1;
+    end
+
+    local bag1, bag2 = item1:GetBagID(), item2:GetBagID();
+    if bag1 ~= bag2 then
+        return bag1 > bag2;
+    end
+
+    local id1, id2 = item1:GetID(), item2:GetID();
+    return id1 < id2;
+end
+
+local function CollectItems(self)
+    local itemsToLayout = {};
+    for _, itemButton in self:EnumerateValidItems() do
+        if not itemButton.isExtended then
+            table.insert(itemsToLayout, itemButton);
+        end
+    end
+
+    return itemsToLayout
+end
+
+hooksecurefunc(ContainerFrame1, "UpdateItemLayout", function(self)
+    AnchorUtil.GridLayout(CollectItems(self), self:GetInitialItemAnchor(), self:GetAnchorLayout())
+end)
+hooksecurefunc(ContainerFrameCombinedBags, "UpdateItemLayout", function(self)
+    local items = CollectItems(self)
+    table.sort(items, SortItemsByExtendedState)
+    AnchorUtil.GridLayout(items, self:GetInitialItemAnchor(), self:GetAnchorLayout())
+end)
 hooksecurefunc("ContainerFrame_GenerateFrame", function(frame, _, id)
     if id == 0 and not IsAccountSecured() then
-        local BACKPACK_DEFAULT_TOPHEIGHT = 256
-        local BACKPACK_BASE_HEIGHT = 256
-        local BACKPACK_MONEY_OFFSET_DEFAULT = -231
-
-        local name = frame:GetName()
-        local bgTextureTop = _G[name.."BackgroundTop"]
-        bgTextureTop:SetHeight(BACKPACK_DEFAULT_TOPHEIGHT)
-        bgTextureTop:SetTexCoord(0, 1, 0, 1)
-
-        _G[name.."MoneyFrame"]:SetPoint("TOPRIGHT", frame, "TOPRIGHT", -2, BACKPACK_MONEY_OFFSET_DEFAULT)
-        _G[name.."AddSlotsButton"]:Hide()
-
-        _G[name.."BackgroundMiddle1"]:Hide()
-        _G[name.."BackgroundMiddle2"]:Hide()
-        _G[name.."BackgroundBottom"]:Hide()
-        BACKPACK_HEIGHT = BACKPACK_BASE_HEIGHT
-        frame:SetHeight(BACKPACK_HEIGHT)
-        ManageBackpackTokenFrame(frame)
-
-        _G[name.."Item1"]:Hide()
-        _G[name.."Item2"]:Hide()
-        _G[name.."Item3"]:Hide()
-        _G[name.."Item4"]:Hide()
+        frame.AddSlotsButton:Hide()
+        ContainerFrame1Item1:Hide()
+        ContainerFrame1Item2:Hide()
+        ContainerFrame1Item3:Hide()
+        ContainerFrame1Item4:Hide()
     end
 end)
