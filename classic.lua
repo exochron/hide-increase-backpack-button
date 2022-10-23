a11acb7cccf19a937777e5355e9f6b796f12b065
exochron <exochron@freenet.de>
2022-10-23 21:51:52 +0200

    - classic: hooked handling to avoid taint erros - retail: new dynamic handling

--- classic.lua
+++ classic.lua
@@ -1,2 +1,10 @@
 -- see https://github.com/tomrus88/BlizzardInterfaceCode/blob/classic/Interface/FrameXML/ContainerFrame.lua#L31
-ADVERTISE_SLOTS_LEVEL = 300
+
+if not InCombatLockdown() then
+    ContainerFrame1AddSlotsButton:Hide()
+end
+hooksecurefunc(ContainerFrame1AddSlotsButton, "SetShown", function(self, show)
+    if show and not InCombatLockdown() then
+        self:Hide()
+    end
+end)
--- retail.lua
+++ retail.lua
@@ -1,12 +1,10 @@
+local ITEM_SPACING_Y = 5;
+
 local f = CreateFrame("Frame")
 f:RegisterEvent("PLAYER_ENTERING_WORLD")
 f:SetScript("OnEvent", function()
     C_CVar.SetCVarBitfield("closedInfoFrames", LE_FRAME_TUTORIAL_BAG_SLOTS_AUTHENTICATOR, true)
 end)
-
-ContainerFrame1.GetRows = function()
-    return IsAccountSecured() and 5 or 4
-end
 
 local function SortItemsByExtendedState(item1, item2)
     local extended1, extended2 = item1:IsExtended(), item2:IsExtended();
@@ -22,32 +20,44 @@ local function SortItemsByExtendedState(item1, item2)
     local id1, id2 = item1:GetID(), item2:GetID();
     return id1 < id2;
 end
-
 local function CollectItems(self)
     local itemsToLayout = {};
     for _, itemButton in self:EnumerateValidItems() do
-        if not itemButton.isExtended then
+        if itemButton:IsExtended() then
+            itemButton:Hide()
+        else
             table.insert(itemsToLayout, itemButton);
         end
     end
 
     return itemsToLayout
 end
+local function UpdateItemLayout(self)
+    if not IsAccountSecured() and not InCombatLockdown() then
+        self.AddSlotsButton:Hide()
+        local items = CollectItems(self)
+        table.sort(items, SortItemsByExtendedState)
+        AnchorUtil.GridLayout(items, self:GetInitialItemAnchor(), self:GetAnchorLayout())
+    end
+end
+hooksecurefunc(ContainerFrame1, "UpdateItemLayout", UpdateItemLayout)
+hooksecurefunc(ContainerFrameCombinedBags, "UpdateItemLayout", UpdateItemLayout)
 
-hooksecurefunc(ContainerFrame1, "UpdateItemLayout", function(self)
-    AnchorUtil.GridLayout(CollectItems(self), self:GetInitialItemAnchor(), self:GetAnchorLayout())
+hooksecurefunc(ContainerFrame1, "UpdateFrameSize", function(self)
+    if not IsAccountSecured() and not InCombatLockdown() then
+        local height = self:CalculateHeight()
+        height = height - self.Items[1]:GetHeight() - ITEM_SPACING_Y
+        self:SetSize(self:CalculateWidth(), height)
+    end
 end)
-hooksecurefunc(ContainerFrameCombinedBags, "UpdateItemLayout", function(self)
-    local items = CollectItems(self)
-    table.sort(items, SortItemsByExtendedState)
-    AnchorUtil.GridLayout(items, self:GetInitialItemAnchor(), self:GetAnchorLayout())
-end)
-hooksecurefunc("ContainerFrame_GenerateFrame", function(frame, _, id)
-    if id == 0 and not IsAccountSecured() then
-        frame.AddSlotsButton:Hide()
-        ContainerFrame1Item1:Hide()
-        ContainerFrame1Item2:Hide()
-        ContainerFrame1Item3:Hide()
-        ContainerFrame1Item4:Hide()
+hooksecurefunc(ContainerFrameCombinedBags, "UpdateFrameSize", function(self)
+    if not IsAccountSecured() and not InCombatLockdown() then
+        local rows = math.ceil((self:GetBagSize() - 4) / self:GetColumns())
+        if rows < self:GetRows() then
+            local itemsHeight = (rows * self.Items[1]:GetHeight()) + ((rows - 1) * ITEM_SPACING_Y);
+            local height = itemsHeight + self:GetPaddingHeight() + self:CalculateExtraHeight();
+
+            self:SetSize(self:CalculateWidth(), height)
+        end
     end
 end)
