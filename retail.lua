9cb5cea9d1732c82c3a03085e549315daac122a2
exochron <exochron@gamerstate.de>
2022-10-23 22:04:39 +0200

    2.0
    Reworked handling for classics and retail 10.0. This update should avoid any taint errors. The plus might be visible in combat though.

--- retail.lua
+++ retail.lua
@@ -1,3 +1,4 @@
+local ITEM_SPACING_Y = 5;
 
 local f = CreateFrame("Frame")
 f:RegisterEvent("PLAYER_ENTERING_WORLD")
@@ -5,30 +6,58 @@ f:SetScript("OnEvent", function()
     C_CVar.SetCVarBitfield("closedInfoFrames", LE_FRAME_TUTORIAL_BAG_SLOTS_AUTHENTICATOR, true)
 end)
 
-hooksecurefunc("ContainerFrame_GenerateFrame", function(frame, _, id)
-    if id == 0 and not IsAccountSecured() then
-        local BACKPACK_DEFAULT_TOPHEIGHT = 256
-        local BACKPACK_BASE_HEIGHT = 256
-        local BACKPACK_MONEY_OFFSET_DEFAULT = -231
+local function SortItemsByExtendedState(item1, item2)
+    local extended1, extended2 = item1:IsExtended(), item2:IsExtended();
+    if extended1 ~= extended2 then
+        return not extended1;
+    end
 
-        local name = frame:GetName()
-        local bgTextureTop = _G[name.."BackgroundTop"]
-        bgTextureTop:SetHeight(BACKPACK_DEFAULT_TOPHEIGHT)
-        bgTextureTop:SetTexCoord(0, 1, 0, 1)
+    local bag1, bag2 = item1:GetBagID(), item2:GetBagID();
+    if bag1 ~= bag2 then
+        return bag1 > bag2;
+    end
 
-        _G[name.."MoneyFrame"]:SetPoint("TOPRIGHT", frame, "TOPRIGHT", -2, BACKPACK_MONEY_OFFSET_DEFAULT)
-        _G[name.."AddSlotsButton"]:Hide()
+    local id1, id2 = item1:GetID(), item2:GetID();
+    return id1 < id2;
+end
+local function CollectItems(self)
+    local itemsToLayout = {};
+    for _, itemButton in self:EnumerateValidItems() do
+        if itemButton:IsExtended() then
+            itemButton:Hide()
+        else
+            table.insert(itemsToLayout, itemButton);
+        end
+    end
 
-        _G[name.."BackgroundMiddle1"]:Hide()
-        _G[name.."BackgroundMiddle2"]:Hide()
-        _G[name.."BackgroundBottom"]:Hide()
-        BACKPACK_HEIGHT = BACKPACK_BASE_HEIGHT
-        frame:SetHeight(BACKPACK_HEIGHT)
-        ManageBackpackTokenFrame(frame)
+    return itemsToLayout
+end
+local function UpdateItemLayout(self)
+    if not IsAccountSecured() and not InCombatLockdown() then
+        self.AddSlotsButton:Hide()
+        local items = CollectItems(self)
+        table.sort(items, SortItemsByExtendedState)
+        AnchorUtil.GridLayout(items, self:GetInitialItemAnchor(), self:GetAnchorLayout())
+    end
+end
+hooksecurefunc(ContainerFrame1, "UpdateItemLayout", UpdateItemLayout)
+hooksecurefunc(ContainerFrameCombinedBags, "UpdateItemLayout", UpdateItemLayout)
 
-        _G[name.."Item1"]:Hide()
-        _G[name.."Item2"]:Hide()
-        _G[name.."Item3"]:Hide()
-        _G[name.."Item4"]:Hide()
+hooksecurefunc(ContainerFrame1, "UpdateFrameSize", function(self)
+    if not IsAccountSecured() and not InCombatLockdown() then
+        local height = self:CalculateHeight()
+        height = height - self.Items[1]:GetHeight() - ITEM_SPACING_Y
+        self:SetSize(self:CalculateWidth(), height)
+    end
+end)
+hooksecurefunc(ContainerFrameCombinedBags, "UpdateFrameSize", function(self)
+    if not IsAccountSecured() and not InCombatLockdown() then
+        local rows = math.ceil((self:GetBagSize() - 4) / self:GetColumns())
+        if rows < self:GetRows() then
+            local itemsHeight = (rows * self.Items[1]:GetHeight()) + ((rows - 1) * ITEM_SPACING_Y);
+            local height = itemsHeight + self:GetPaddingHeight() + self:CalculateExtraHeight();
+
+            self:SetSize(self:CalculateWidth(), height)
+        end
     end
 end)
